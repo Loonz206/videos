d929e31bb55bb1b0a70d9ec4e438fbff30a1c827
Lenny Peters <mr.lennypeters@gmail.com>
2022-04-13 20:54:37 -0700

    feat: 🎸 adding context to store data
    another state management thing

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import { SearchContext } from "./App";
 
 const VideoDetail = ({ video }) => {
+  // literally testing grabbing context that was at the App level
+  const value = useContext(SearchContext);
+  console.log("VALUE", value);
   if (!video) {
     return <div>Loading....</div>;
   }
